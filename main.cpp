c5d5f1dfd380a3f3fac4ae638db108ed13a71917
timothyw8 <timwang2002@gmail.com>
2025-11-04 17:06:46 -0500

    database city names stored in vector

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <map>
 #include <vector>
+#include <sstream>
 
 using namespace std;
 
@@ -11,6 +12,12 @@ struct Edge {
     double cost;
     double time;
 
+    Edge() {
+        to = "";
+        type = "";
+        cost = 0.0;
+        time = 0.0;
+    }
     Edge(string t, string ty, double co, double ti) {
         to = t;
         type = ty;
@@ -21,9 +28,16 @@ struct Edge {
 
 class Graph {
     map<string, vector<Edge>> edges;
+    vector<string> cities;
 public:
     void addEdge(string src, string dst, string type, double cost, double time) {
         edges[src].push_back(Edge(dst, type, cost, time));
+    }
+    void addCity(string city) {
+        cities.push_back(city);
+    }
+    vector<string> getCities() {
+        return cities;
     }
 
 
@@ -35,12 +49,26 @@ int main() {
     cout<<"TEST" << endl;
     cout<<"this is from marisa"<<endl;
 
-    ifstream file("us_cities.json");
+    ifstream file("uscities.csv");
     if (!file.is_open()) {
         cout<<"File could not be opened"<<endl;
         return 1;
     }
 
+    Graph temp;
+    string line;
+    getline(file,line);
+;   while (getline(file, line)) {
+        stringstream ss(line);
+        string city;
+        if (getline(ss,city,',')) {
+            temp.addCity(city);
+        }
+    }
+    vector<string> cities = temp.getCities();
+    for (auto city: cities) {
+        cout<<city<<endl;
+    }
 
     return 0;
 }
