8a01a1992b94e7038cbdce9b7fb1202390795424
Aryan Wagh <aryan.wagh05@gmail.com>
2025-11-04 16:17:18 -0500

    database aryan

--- main.cpp
+++ main.cpp
@@ -1,9 +1,46 @@
 #include <iostream>
+#include <fstream>
+#include <map>
+#include <vector>
+
 using namespace std;
+
+struct Edge {
+    string to;
+    string type;
+    double cost;
+    double time;
+
+    Edge(string t, string ty, double co, double ti) {
+        to = t;
+        type = ty;
+        cost = co;
+        time = ti;
+    }
+};
+
+class Graph {
+    map<string, vector<Edge>> edges;
+public:
+    void addEdge(string src, string dst, string type, double cost, double time) {
+        edges[src].push_back(Edge(dst, type, cost, time));
+    }
+
+
+
+};
 
 int main() {
     cout<<"Marisa puppy fart!"<<endl;
     cout<<"TEST" << endl;
     cout<<"this is from marisa"<<endl;
+
+    ifstream file("us_cities.json");
+    if (!file.is_open()) {
+        cout<<"File could not be opened"<<endl;
+        return 1;
+    }
+
+
     return 0;
 }
